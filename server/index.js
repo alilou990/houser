ecee8f6d80fbe502c173c4bd82c282333c5cb246
Ali <eight.falling.stars@gmail.com>
2019-08-30 11:07:52 -0600

    built out the server and started the database

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const cors = require('cors')
+
+//controller file
+const ctrl = require('./controller')
+
+//env variables
+const {
+    SERVER_PORT,
+    CONNECTION_STRING
+} = process.env 
+
+//setting up app
+const app = express();
+
+//tlm
+app.use(express.json())
+app.use(cors())
+
+//massive connection
+massive(CONNECTION_STRING)
+    .then(dbInstance => {
+        app.set('db', dbInstance)
+        console.log('database connected! 👻')
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+//end points
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log('server running! 👾')
+})
