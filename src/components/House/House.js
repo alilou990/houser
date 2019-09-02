85bc074037387b9fd8439cbc76ad49d1e0e34bed
Ali <eight.falling.stars@gmail.com>
2019-09-02 09:09:29 -0600

    worked on css

--- src/components/House/House.js
+++ src/components/House/House.js
@@ -10,15 +10,15 @@ const House = (props) => {
                 </div>
                 <div className='house-info-one'>
                     
-                    <label>Property Name: {props.name}</label>
-                    <label>Address: {props.address}</label>
-                    <label>City: {props.city}</label>
-                    <label>State: {props.state}</label>
-                    <label>Zip: {props.zip}</label>
+                    <label className='house-info-text'>Property Name: {props.name}</label>
+                    <label className='house-info-text'>Address: {props.address}</label>
+                    <label className='house-info-text'>City: {props.city}</label>
+                    <label className='house-info-text'>State: {props.state}</label>
+                    <label className='house-info-text'>Zip: {props.zip}</label>
                 </div>
                 <div className='house-info-two'>
-                    <label>Monthly Mortgage: {props.mortgage}</label>
-                    <label>Monthly Rent: {props.rent}</label>
+                    <label className='house-info-text'>Monthly Mortgage: {props.mortgage}</label>
+                    <label className='house-info-text'>Monthly Rent: {props.rent}</label>
                     <button onClick={() => props.deleteHouse(props.house.id)}>Delete</button>
                 </div>
             </div>
